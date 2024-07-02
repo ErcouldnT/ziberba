0d9e4fa3d092d230ef00fde6a9a5145abfaed718
Ercode Techoglu <ercode@pm.me>
2024-07-02 07:55:35 +0300

    Add nativewind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-black">
+      <Text className="text-green-300 text-xl font-semibold">Merhaba!</Text>
+    </View>
+  );
+}
